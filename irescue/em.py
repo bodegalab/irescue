4940f89754a2f724d98eff667b746ca3460447e4
Benedetto Polimeni <polimeni@ingm.org>
2023-11-10 13:08:01 +0000

    optimize EM speed

--- irescue/em.py
+++ irescue/em.py
@@ -5,17 +5,20 @@ def e_step(matrix, counts):
     Performs E-step of EM algorithm: proportionally assigns reads to features
     based on relative feature abundances.
     """
-    rows, cols = matrix.shape
-    out = np.empty(matrix.shape, dtype=float)
-    for row in range(rows):
-        for col in range(cols):
-            # each matrix value is divided by the sum of its column and
-            # multiplied by the current feature value
-            out[row,col] = (
-                matrix[row,col]
-                / sum([counts[i] for i in np.where(matrix[row] > 0)[0]])
-                * counts[col]
-            )
+    #rows, cols = matrix.shape
+    #out = np.empty(matrix.shape, dtype=float)
+    #for row in range(rows):
+    #    for col in range(cols):
+    #        # each matrix value is divided by the sum of its column and
+    #        # multiplied by the current feature value
+    #        out[row,col] = (
+    #            matrix[row,col]
+    #            / sum([counts[i] for i in np.where(matrix[row] > 0)[0]])
+    #            * counts[col]
+    #        )
+
+    colsums = (matrix * counts).sum(axis=1)[:, np.newaxis]
+    out = matrix / colsums * counts
     return(out)
 
 def m_step(matrix):
@@ -23,10 +26,14 @@ def m_step(matrix):
     Performs M-step of EM algorithm: calculates feature abundances from read
     counts proportionally distributed to features.
     """
-    nFeatures = matrix.shape[1]
-    counts = list()
-    for i in range(nFeatures):
-        counts.append(matrix[:, i].sum() / matrix.sum())
+    #nFeatures = matrix.shape[1]
+    #counts = list()
+    #for i in range(nFeatures):
+    #    counts.append(matrix[:, i].sum() / matrix.sum())
+    
+    #total = matrix.sum()
+    #counts = [matrix[:, x].sum() / total for x in range(nFeatures)]
+    counts = matrix.sum(axis=0) / matrix.sum()
     return(counts)
 
 def run_em(matrix, cycles=100):
@@ -46,10 +53,12 @@ def run_em(matrix, cycles=100):
     out : list
         Optimized relative feature abundances.
     """
-    # let the sum of counts of features be 1
-    # (it will be multiplied by the real count later)
+    
+    # calculate initial estimation of relative abundance.
+    # (let the sum of counts of features be 1,
+    # will be multiplied by the real UMI count later)
     nFeatures = matrix.shape[1]
-    counts = [1 / nFeatures] * nFeatures
+    counts = np.array([1 / nFeatures] * nFeatures)
 
     # run EM for n cycles
     for _ in range(cycles):
